fb2a4c3532ecdde8ce1b2085cfc42954c9128ab3
Alejandro VIlla <alexandros17.sk@gmail.com>
2020-10-18 19:13:17 -0500

    configuracion de variables d e entorno

--- database/sequelize/config.js
+++ database/sequelize/config.js
@@ -1,24 +1,23 @@
+require('dotenv').config();
+
 const Sequelize = require("sequelize");
 
-const dbHost = "localhost";
-const dbName = "delilah_resto"; // Put here your data
-//const dbPort = "3306";
-const dbUser = "root"; // Put here your data
-const password = "mrobot" // Put here your data
+const dbHost = process.env.DB_HOST;
+const dbName = process.env.DB_NAME; 
+const dbUser = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
 
 
 
 const sequelize = new Sequelize(dbName, dbUser, password, {
     host: dbHost,
-    dialect: "mariadb"
+    dialect:  process.env.DB_DIALECT
 });
 
 sequelize.authenticate().then(() => {
     console.log('Database connected.');
 }).catch(err => {
     console.error('Error de conexion:', err);
-})/*.finally(() => {
-    sequelize.close();
-});*/ 
+})
 
 module.exports = { sequelize };
